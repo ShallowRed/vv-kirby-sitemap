13676601fdda593def152d94580373456a6d6e05
ShallowRed <48523123+ShallowRed@users.noreply.github.com>
2024-06-14 14:51:09 +0200

    Remove styles

--- snippets/blocks/sitemap.php
+++ snippets/blocks/sitemap.php
@@ -3,16 +3,17 @@
 <?php $attrs ??= ['class' => 'sitemap']; ?>
 <ul <?php echo attr($attrs);?>>
     <?php foreach ($sitemapPages ??= $site->pages()->inSitemap() as $page) : ?>
-      <li class="<?php echo $depth === 0 ? 'mt-4' : 'mt-2';?>">
+      <li data-depth="<?php echo $depth;?>">
       <?php echo Html::a(
           $page->url(),
-          [Html::tag('p', $page->title(), ['class' =>  $depth === 0 ? 'text-xl' : 'text-lg'])]
+          [Html::tag('p', $page->title())]
       )?>
       <?php if ($page->hasChildren()) : ?>
         <?php snippet(
             'blocks/sitemap',
             [
-              'sitemapPages' => $page->children()->inSitemap(), 'attrs' => ['class' => 'pl-8'], 'depth' => $depth + 1
+              'sitemapPages' => $page->children()->inSitemap(),
+              'depth' => $depth + 1
             ]
         ) ?>
       <?php endif;?>
